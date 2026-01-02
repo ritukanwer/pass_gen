b14ddc69911be6dafc39e98f22a431e08ac6419b
Ritu <ritunarukarajput.21@gmail.com>
2026-01-02 15:34:48 +0530

    password_generator

--- password_gen.py
+++ password_gen.py
@@ -2,13 +2,13 @@ import string
 import random
 if __name__== '__main__':
     s1 = string.ascii_lowercase
-    print(s1)
+    # print(s1)
     s2 = string.ascii_uppercase
-    print(s2)
+    # print(s2)
     s3 = string.digits
-    print(s3)
+    # print(s3)
     s4 = string.punctuation
-    print(s4)
+    # print(s4)
 
 
     passlen = int(input("enter your passwords lenght:-"))
